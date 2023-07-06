a0bcf07f1c1aebae41b7ca782ae6ebf3e60f4f75
anton <antonhdo@gmail.com>
2023-07-05 21:25:31 -0700

    Tailwind css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import background from './assets/blackwhite design.png';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='bg-gradient-to-tr from-red-100 to-blue-500 h-screen w-full  relative'>
+        <img className='w-full h-full object-cover absolute mix-blend-overlay' src={background} />
+        <div className='p-24 flex justify-center items-center h-full border-2 border-rose-500'>
+          <div className=' flex flex-col items-center justify-center'>
+
+            <h1 className=" text-slate-200 text-6xl font-bold">Make Your Fashion Dreams Come True</h1>
+            <h2 className='text-slate-200 text-4xl mt-5'>Sign up today</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
