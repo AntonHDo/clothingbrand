bafeae6bf9c8e2fa308f61de74429f7b6fb0d3f1
anton <antonhdo@gmail.com>
2023-07-22 19:23:57 -0700

    Getting stuff to work more

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import background from './assets/blackwhite design.png';
+import { useModal } from './Context/Modal';
+import OpenModalButton from './Components/OpenModalButton';
+import SignUpModal from './Components/SignUpModal';
 
 const App = () => {
+  const { closeModal } = useModal
   return (
     <div>
       <div className='bg-gradient-to-tr from-red-100 to-blue-500 h-screen w-full  relative'>
@@ -11,20 +15,34 @@ const App = () => {
           <div className=' flex flex-col items-center justify-center'>
 
             <h1 className=" text-slate-100 text-6xl font-bold relative">Make Your Fashion Dreams Come True</h1>
-            <h2 className='text-slate-100 text-4xl mt-5 relative cursor-pointer underline'>Sign up today</h2>
+
+            <h2 className='text-slate-100 text-4xl mt-5 relative cursor-pointer underline'><OpenModalButton modalComponent={<SignUpModal />}
+              buttonText="Sign up today" /></h2>
           </div>
         </div>
       </div>
-      <footer className='flex flex-row justify-around items-center '>
-        <div>
+      <footer className='flex flex-row justify-around items-center  '>
+
+
+        <div className='flex flex-col justify-center items-center basis-1/3'>
           About
+          <div className=''>
+            We are aiming to help push those in need of a new career path in Fashion! We are are an established bootcamp that focuses on what people actaully do on the job
+          </div>
         </div>
-        <div>
+        <div className='flex flex-col justify-center items-center basis-1/3' >
           Mission
+          <div className=''>
+            We are aiming to help push those in need of a new career path in Fashion! We are are an established bootcamp that focuses on what people actaully do on the job
+          </div>
         </div>
-        <div>
+        <div className='flex flex-col justify-center items-center basis-1/3' >
           FAQ
+          <div className=''>
+            We are aiming to help push those in need of a new career path in Fashion! We are are an establissdasdasdasdashed bootcamp that focuses on what people actaully do on the jobffffffffffffffffffffffffffffffffffffffffssssssssssssssssssssssssssssssssssssssssss
+          </div>
         </div>
+
       </footer>
     </div>
   );
---
+++ src/Components/SignUpModal/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useModal } from '../../Context/Modal'
+
+const SignUpModal = () => {
+  const { closeModal } = useModal
+  return (
+    <div className='flex bg-red-400/[.5] p-10 '>
+      <div className='text-blue-100 text-center'>hisasaaaaaaaaaaaaaaaa</div>
+    </div>
+  )
+}
+
+export default SignUpModal
